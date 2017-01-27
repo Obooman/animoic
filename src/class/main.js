697a80f85175993556c985e51588b8adf8765905
Oboo Chin <cheng.12@live.cn>
2017-01-27 15:50:01 +0800

    Update main.js

--- src/class/main.js
+++ src/class/main.js
@@ -1,5 +1,27 @@
 class Bezier{
+	constructor(SVGPath){
+		// M0,0 C0.2,0.3 0.8,0.9 1,1
+		this.bezierArray = this.handlePath(SVGPath);
+	}
 
+	handlePath = (SVGPath) => {
+		var pathSplit = SVGPath.split(' ');
+
+		pathSplit.forEach((path,index) => {
+			// current timing function range from path.x to index+1 path.x
+			
+			if( /M/.test(path) ){
+				// moveTo
+
+			} else if( /C/.test(path) ){
+				// curveTo
+
+			} else if( /L/.test(path) ){
+				// lineTo
+
+			}
+		})
+	}
 }
 
 // down and up timing function
